ca4e0df003b35eea41f91139b06e2de1dcd39618
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-05-12 12:03:27 +0530

    Added: Ability to develop rest of the CRUD Service Methods with MySQL Database

--- src/main/java/com/bridgelabz/employeepayrollappdevelopment/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollappdevelopment/model/EmployeePayrollData.java
@@ -26,31 +26,22 @@ public @Data class EmployeePayrollData {
     private String profilePic;
 
     @ElementCollection
-    @CollectionTable(name = "employee_department",
-            joinColumns = @JoinColumn(name ="id"))
+    @CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
     @Column(name = "departments")
     private List<String> departments;
 
-    public EmployeePayrollData() {
-
-    }
-
-    public EmployeePayrollData(EmployeePayrollDTO employeePayrollDTO){
+    public EmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
         this.updateEmployeePayollData(employeePayrollDTO);
     }
 
-    private void updateEmployeePayollData(EmployeePayrollDTO employeePayrollDTO) {
-    }
-
-    public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
-        this.employeeId = employeeId;
+    public void updateEmployeePayollData( EmployeePayrollDTO employeePayrollDTO) {
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
         this.gender = employeePayrollDTO.gender;
-        this.startDate = employeePayrollDTO.startDate;
         this.note = employeePayrollDTO.note;
+        this.startDate = employeePayrollDTO.startDate;
         this.profilePic = employeePayrollDTO.profilePic;
         this.departments = employeePayrollDTO.departments;
-    }
 
+    }
 }
